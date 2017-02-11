31e3af89b9ada195de8b521ed5c7d44d46625c11
alynnefs <alynne@lit.ifce.edu.br>
2017-02-10 23:23:02 -0300

    finalizado

--- kalman.c
+++ kalman.c
@@ -1,17 +1,56 @@
+/*
+ * kalman.c - Firmware de PIC18 para executar um filtro de Kalman.
+ * 
+ * Entrada: dois vetores do tipo 'float const' presentes no arquivo in.h
+ * Saída: índice (para facilitar testes), valor com ruído e valor filtrado.
+ *        Os dois últimos são arredondados com duas casas decimais.
+ *
+ * Modo de uso: os valores dos vetores existentes em in.h (com ruído) são
+ *              utilizados para calcular a iteração futura, de forma que a saída
+ *              seja o mais próximo possível do valor real.
+ *              O valor real é gerado pelo arquivo valores.py.
+ *              Os dados são obtidos através do arquivo grafico.py, que lê os
+ *              dados da serial e armazena valores de entrada e saída em 
+ *              arquivos diferentes.
+ *              Os gráficos são gerados pelo arquivo gera_grafico.py, com
+ *              parâmetros exemplificados no README.
+ *              Todos os arquivos com exceção do in.h (que é gerado pelo arquivo
+ *              valores.py) estão presentes no link abaixo.
+ *              https://github.com/alynnefs/kalman_filter_pic
+ *              OBS: in.h e kalman.c devem estar no mesmo nível de diretório.
+ *                   MPLABX e XC8 foram utilizados para desenvolvimento e compilação.
+ * 
+ * Algoritmo desenvolvido para a cadeira de Sistemas Embarcados do curso de
+ * Engenharia de Computação do IFCE
+ * 
+ * Copyright (C) 2017 Alynne Ferreira <alynneferreiras@gmail.com>
+ * Copyright (C) 2017 Mateus Rodrigues <mateus.fr26@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public
+ * License version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License version 3 for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License version 3 along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+
+// As configurações abaixo foram geradas pela IDE ao setar alguns registradores.
+// Os registradores que não foram setados possuem valores default
 //// CLOCK DE 8MHz, CLOCK INTERNO
-//#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
-//#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
-//#pragma config USBDIV = 1       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
-//#pragma config FOSC = INTOSC_XT // Oscillator Selection bits (Internal oscillator, XT used by USB (INTXT))
-//#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
-//#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
-//// CLOCK DE 48MHz, CRISTAL EXTERNO COM PLL
-#pragma config PLLDIV = 5       // PLL Prescaler Selection bits (Divide by 5 (20 MHz oscillator input))
+#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
 #pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
 #pragma config USBDIV = 1       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
-#pragma config FOSC = HSPLL_HS  // Oscillator Selection bits (HS oscillator, PLL enabled (HSPLL))
+#pragma config FOSC = INTOSC_XT // Oscillator Selection bits (Internal oscillator, XT used by USB (INTXT))
 #pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
 #pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
 //------------------------------------------------------------------------------
 // CONFIG2L
 #pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
@@ -65,49 +104,81 @@
 #pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)//
 //// #pragma config statements should precede project file includes.
 //// Use project enums instead of #define for ON and OFF.
-#define _XTAL_FREQ 8000000
+
+// fim das configurações geradas pela IDE
+
+#define _XTAL_FREQ 8000000 // clock interno
 #include <xc.h>
 #include <stdio.h>
 #include <stdlib.h>   
 #include <string.h>
-#include "in.h"
+#include "in.h" // entrada de dados
 
 
+// Valores usados pra cálculo. Foram obtidos com testes.
 double Q = 0.0; // erro estimado no processo
 double R = 0.2; // erro estimado nas medidas
 double P = 1.0, X = 0.0, K;
 // P: estimativa de covariância inicial, X: estimativa inicial do estado, K: variavel auxiliar
 double result;
-double result2;
 
-// atualizacao de medidas com base no filtro de kalman
+// variáveis auxiliares
+unsigned int i = 0; // índice do vetor
+unsigned int aux = 0; // variável que ajuda a definir qual laço será executado
 
+/* Atualização de medidas com base no filtro de kalman
+ * Origem da função:
+ * http://www.dyadica.co.uk/very-simple-kalman-in-c/
+ * acesso em janeiro de 2017
+ * Obs: modificada de C# para C
+ */
 void measurementUpdate() {
     K = (P + Q) / (P + Q + R);
     P = R * (P + Q) / (R + P + Q);
 }
 
-// soma com k anterior
 
+/* Soma com k anterior
+ * Origem da função:
+ * http://www.dyadica.co.uk/very-simple-kalman-in-c/
+ * acesso em janeiro de 2017
+ * Obs: modificada de C# para C
+ */
 double update(double measurement) {
-    //double result;
     result = X + (measurement - X) * K;
     measurementUpdate(); //atualizacao de medidas
     X = result;
     return result;
 }
 
+/* Inicialização da UART.
+ * Origem da função:
+ * https://electrosome.com/uart-pic-microcontroller-mplab-xc8/
+ * acesso em fevereiro de 2017
+ * Obs: modificado para tornar mais específico
+ */
 void UART_Init(const long int baudrate) {
-    unsigned int x;
-    x = (_XTAL_FREQ / baudrate / 64) - 1; //SPBRG for Low Baud Rate
-    SPBRG = x; //Writing SPBRG Register
+    unsigned int y; // 
+    y = (_XTAL_FREQ / baudrate / 4) - 1; //SPBRG for Low Baud Rate
+    // obs sobre y: equação modificada de acordo com o datasheet, de forma que possa funcionar com baud rate de 115200
+    SPBRG = y; //Writing SPBRG Register
     SYNC = 0; //Setting Asynchronous Mode, ie UART
     SPEN = 1; //Enables Serial Port
     TRISC7 = 1; //As Prescribed in Datasheet
     TRISC6 = 1; //As Prescribed in Datasheet
     CREN = 1; //Enables Continuous Reception
     TXEN = 1; //Enables Transmission
+    
+    // configuração pra aumentar o baud rate 
+    BRGH = 1;
+    BRG16 = 1;
 }
+
+/* Envio pela serial
+ * Origem da função:
+ * https://electrosome.com/uart-pic-microcontroller-mplab-xc8/
+ * acesso em fevereiro de 2017
+ */
 
 void putch(unsigned char data) {
     while (!PIR1bits.TXIF); // wait until the transmitter is ready
@@ -115,60 +186,39 @@ void putch(unsigned char data) {
 }
 
 void main(void) {
-    UART_Init(9600);
+    UART_Init(115200); // inicialização da UART
     //// CLOCK DE 8MHz, CLOCK INTERNO
-    OSCCONbits.IRCF = 7; // oscilador interno a 8MHz
-    //OSCCONbits.SCS = 2; // oscilador interno
-    // CLOCK DE 48MHz, CRISTAL EXTERNO, COM PLL
-    OSCCONbits.SCS = 2; // oscilador interno
+    OSCCONbits.IRCF = 7; // escolhe oscilador interno a 8MHz
+    OSCCONbits.SCS = 2; // 'ativa' oscilador interno
 
-    TMR0L = 99;
-
-    T0CONbits.T08BIT = 1;
-    T0CONbits.T0CS = 0;
-    T0CONbits.PSA = 0;
-    T0CONbits.T0PS = 6;
-    T0CONbits.TMR0ON = 1;
-    
-    TRISB = 0;
-    LATB = 0xFF;
-
-    unsigned int i = 0;
-    unsigned int aux = 0;
-
-        while(1){
+    while(1){
+       // todas as variáveis abaixo são globais, com exceção de NUM_ANS, DATA1 e DATA2
         
-        LATBbits.LATB3 = 1;
-        while (!INTCONbits.TMR0IF);
-        INTCONbits.TMR0IF = 0;
-       // printf('v'); //inicio voltage
-        if(i<NUM_AMS && aux == 0){
-            result = update(DATA1[i]);
-            printf("%d\t%.2f\t%.2f\n\r", i, DATA1[i], result);
-            i++;
-            LATBbits.LATB4 = !LATBbits.LATB4;
-               if(i == NUM_AMS){
-                Q = 0.0;
-                R = 0.2;
+        //voltage
+        if(i<NUM_AMS && aux == 0){ // aux == 0 executa o DATA[]
+            result = update(DATA1[i]); // executa o filtro de acordo com o valor atual
+            printf("%d\t%.2f\t%.2f\n\r", i, DATA1[i], result); // envia índice, valor atual e valor depois do filtro
+            i++; // muda o próximo índice
+            
+            if(i == NUM_AMS){ // reinicia as variáveis se o índice for 800 
                 P = 1.0;
                 X = 0.0;
                 K = 0.0;
                 i = 0;
-                aux = 1;
-                LATBbits.LATB4 = 0;
+                aux = 1; // faz com que a próxima iteração ocorra apenas na condição seguinte
+                result = 0;
             }
         }
         
-//            LATBbits.LATB4 = 0;
-
-       // printf('c'); //inicio cannon
-        if(i<NUM_AMS && aux == 1){
-            result2 = update(DATA2[i]);
-            printf("%d\t%.2f\t%.2f\n\r", i, DATA2[i], result2);
-            i++;
-            LATBbits.LATB4 = !LATBbits.LATB4;
-            if(i == NUM_AMS){
-                aux = 2; //para
+        //cannon
+        if(i<NUM_AMS && aux == 1){ // aux == 1 executa o DATA2[]
+            result = update(DATA2[i]); // executa o filtro de acordo com o valor atual
+            printf("%d\t%.2f\t%.2f\n\r", i, DATA2[i], result); // envia índice, valor atual e valor depois do filtro
+            i++; // muda o próximo índice
+            
+            if(i == NUM_AMS){ // se o índice for 800 reinicia as variáveis
+                aux = 2; // aux == 2 para os cálulos relacionados ao filtro de Kalman. O programa permanece em while(1),
+                         // verificando as condições, mas sem executá-las, pois nenhuma será satisfeita
             }
         }
     }
